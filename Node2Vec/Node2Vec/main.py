357a032f63daa211525956c550ff28c693d7d214
Markson-Young <a2246708680@gmail.com>
2022-08-25 16:25:28 +0800

    添加deepwalk

---
+++ Node2Vec/Node2Vec/main.py
@@ -0,0 +1,7 @@
+from utils.args import arg_utils
+
+
+
+if __name__ == '__main__':
+    args = arg_utils()
+    print(args)
---
+++ Node2Vec/Node2Vec/utils/args.py
@@ -0,0 +1,49 @@
+import argparse
+
+def arg_utils():
+    parser = argparse.ArgumentParser(description='Node2Vec args')
+    '''
+    Parses the node2vec arguments.
+    '''
+    # 输入文件：邻接表
+    parser.add_argument('--input', nargs='?', default='karate.edgelist',
+                        help='Input graph path')
+    # 输出文件：节点嵌入表
+    parser.add_argument('--output', nargs='?', default='karate.emb',
+                        help='Embeddings path')
+    # embedding嵌入向量维度
+    parser.add_argument('--dimensions', type=int, default=128,
+                        help='Number of dimensions. Default is 128.')
+    # 随机游走序列长度
+    parser.add_argument('--walk-length', type=int, default=80,
+                        help='Length of walk per source. Default is 80.')
+    # 每个节点生成随机游走序列次数
+    parser.add_argument('--num-walks', type=int, default=10,
+                        help='Number of walks per source. Default is 10.')
+    # word2vec窗口大小，word2vec参数
+    parser.add_argument('--window-size', type=int, default=10,
+                        help='Context size for optimization. Default is 10.')
+    # SGD优化时epoch数量，word2vec参数
+    parser.add_argument('--iter', default=1, type=int,
+                        help='Number of epochs in SGD')
+    # 并行化核数，word2vec参数
+    parser.add_argument('--workers', type=int, default=8,
+                        help='Number of parallel workers. Default is 8.')
+    # 参数p
+    parser.add_argument('--p', type=float, default=1,
+                        help='Return hyperparameter. Default is 1.')
+    # 参数q
+    parser.add_argument('--q', type=float, default=2,
+                        help='Inout hyperparameter. Default is 2.')
+    # 连接是否带权重
+    parser.add_argument('--weighted', dest='weighted', action='store_true',
+                        help='Boolean specifying (un)weighted. Default is unweighted.')
+    parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+    parser.set_defaults(weighted=False)
+
+    # 有向图还是无向图
+    parser.add_argument('--directed', dest='directed', action='store_true',
+                        help='Graph is (un)directed. Default is undirected.')
+    parser.add_argument('--undirected', dest='undirected', action='store_false')
+    parser.set_defaults(directed=False)
+    return parser.parse_args(args=[])
